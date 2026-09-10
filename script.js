2b4e3f5144219142141ff5a05479d3c5d870ef61
infoprabintech-cpu <info.prabintech@gmail.com>
2026-09-10 12:09:58 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -100,9 +100,21 @@ const navMenu = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("show");
+
+    if (navMenu.classList.contains("show")) {
+        menuToggle.textContent = "✕";
+    } else {
+        menuToggle.textContent = "☰";
+    }
 });
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
         navMenu.classList.remove("show");
+    });
+});
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("show");
+        menuToggle.textContent = "☰";
     });
 });
