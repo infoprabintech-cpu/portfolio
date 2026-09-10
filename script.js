2bef02208ca6db6975169316c5d119745a20ab57
infoprabintech-cpu <info.prabintech@gmail.com>
2026-09-10 12:02:16 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -95,3 +95,9 @@ function confirmCVDownload(event) {
         document.body.removeChild(link);
     }
 }
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
